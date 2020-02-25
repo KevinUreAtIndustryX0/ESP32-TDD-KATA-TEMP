c02e9f269272488747ca8ce166c93c63628554fd
Todd Flanders <todd.a.flanders@accenture.com>
2020-02-25 17:01:58 -0500

    fix: network connection

--- components/AppController/appController.c
+++ components/AppController/appController.c
@@ -5,12 +5,18 @@
 #include "networkController.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "nvs_flash.h"
 
 void mainLoop(void){
 }
 
 void init(void){
     ESP_LOGI("Example", "Hello World");
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        nvs_flash_erase();
+        nvs_flash_init();
+    }     
     initLedController();
     initNetworkController();
 }
--- components/NetworkController/networkController.c
+++ components/NetworkController/networkController.c
@@ -3,8 +3,13 @@
 #include "esp_tls.h"
 #include "esp_http_client.h"
 #include "ledController.h"
+#include "esp_netif.h"
+#include "esp_event.h"
 
 void initNetworkController(void) {
+    esp_netif_init();
+    esp_event_loop_create_default();
+
     setLedState(LED_ON);
     example_connect();
     setLedState(LED_OFF);
