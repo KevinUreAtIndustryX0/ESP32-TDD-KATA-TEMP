1e8c184372df5bec57da1c82a75cbdfd343aaaae
Todd Flanders <todd.a.flanders@accenture.com>
2020-02-27 16:50:26 -0500

    stopping point for step-by-step instructions

--- step-1-getting-started/starter/test/main_tests.cpp
+++ step-1-getting-started/starter/test/main_tests.cpp
@@ -13,9 +13,9 @@ FAKE_VOID_FUNC(app_do_work);
 TEST(app_main, calls_logic_init_and_logic_do_work) {
     app_main();
 
-    FAIL() << "TODO: Delete this line, uncomment the following lines, and runWe shouldn't get here.";
-    // EXPECT_EQ(app_init_fake.call_count, 1);
-    // EXPECT_EQ(app_do_work_fake.call_count, 1);
-    // RESET_FAKE(app_init);
-    // RESET_FAKE(app_do_work);
+    // FAIL() << "TODO: Delete this line, uncomment the following lines, and re-run the tests";
+    EXPECT_EQ(app_init_fake.call_count, 1);
+    EXPECT_EQ(app_do_work_fake.call_count, 1);
+    RESET_FAKE(app_init);
+    RESET_FAKE(app_do_work);
 }
