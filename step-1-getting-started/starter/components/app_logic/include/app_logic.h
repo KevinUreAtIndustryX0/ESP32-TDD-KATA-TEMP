832f2b3dbfcbbbaa27ab4433e66f9c9b16b3a365
Todd Flanders <todd.a.flanders@accenture.com>
2020-02-27 14:13:42 -0500

    step 1 starter created

---
+++ step-1-getting-started/starter/components/app_logic/include/app_logic.h
@@ -0,0 +1,8 @@
+#ifndef _APP_LOGIC_H_
+#define _APP_LOGIC_H_
+
+void app_init(void);
+
+void app_do_work(void);
+
+#endif
---
+++ step-1-getting-started/starter/test/main_tests.cpp
@@ -0,0 +1,19 @@
+#include "gtest/gtest.h"
+extern "C" {
+    #include "main.h"
+    #include "app_logic.h"
+}
+#include "fff.h"
+
+DEFINE_FFF_GLOBALS;
+
+FAKE_VOID_FUNC(app_init);
+FAKE_VOID_FUNC(app_do_work);
+
+TEST(app_main, calls_logic_init_and_logic_do_work) {
+    app_main();
+    EXPECT_EQ(app_init_fake.call_count, 1);
+    EXPECT_EQ(app_do_work_fake.call_count, 1);
+    RESET_FAKE(app_init);
+    RESET_FAKE(app_do_work);
+}
